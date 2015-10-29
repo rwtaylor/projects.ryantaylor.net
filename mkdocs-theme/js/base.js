d41a3665b54edf60ae1f860bd6abadf042d4c25a
Ryan W. Taylor <ryan@ryantaylor.net>
2015-10-29 10:24:38 -0700

    Move to document body

--- mkdocs-theme/js/base.js
+++ mkdocs-theme/js/base.js
@@ -15,25 +15,3 @@ $('body').scrollspy({
 $("li.disabled a").click(function() {
     event.preventDefault();
 });
-
-
-/* Change image stuff */
-
-function getCheckedRadioValue(name) {
-    var elements = document.getElementsByName(name);
-    for (var i=0, len=elements.length; i<len; ++i)
-        if (elements[i].checked) return elements[i].value;
-}
-
-function changeURL(root) {
-    var d1 = $("#chrom").find(":selected").attr("value");
-    var d2 = getCheckedRadioValue("var_fill");
-    var d3 = getCheckedRadioValue("aggregation");
-    var d4 = getCheckedRadioValue("padding");
-    var url = (root + "-" + d1 + "-l2-" + d2 + "-" + d3 + "-" + d4 + ".png");
-    $('#plotimage').attr("src", url);
-}
-
-$( "input[type='radio']" ).change(function() {
-  changeURL();
-});
