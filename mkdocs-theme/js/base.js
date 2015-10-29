542c21308784af3fad122fb71da11d82160e647d
Ryan W. Taylor <ryan@ryantaylor.net>
2015-10-28 21:15:56 -0700

    Add js for image changes

--- mkdocs-theme/js/base.js
+++ mkdocs-theme/js/base.js
@@ -17,4 +17,23 @@ $("li.disabled a").click(function() {
 });
 
 
+/* Change image stuff */
 
+function getCheckedRadioValue(name) {
+    var elements = document.getElementsByName(name);
+    for (var i=0, len=elements.length; i<len; ++i)
+        if (elements[i].checked) return elements[i].value;
+}
+
+function changeURL() {
+    var d1 = $("#chrom").find(":selected").attr("value");
+    var d2 = getCheckedRadioValue("var_fill");
+    var d3 = getCheckedRadioValue("aggregation");
+    var d4 = getCheckedRadioValue("padding");
+    var url = ("http://public.ryantaylor.net/figure/04b3-" + d1 + "-l2-" + d2 + "-" + d3 + "-" + d4 + ".png");
+    $('#plotimage').attr("src", url);
+}
+
+$( "input[type='radio']" ).change(function() {
+  changeURL();
+});
